c4c17dfe5a24c330c54e627eb2f4988219e6abd0
newor0599 <rowen778623@gmail.com>
2025-07-25 20:55:10 +0800

    Fix tests

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ from ignis.app import IgnisApp
 from os import path
 
 # Test
-test_flag = True
+test_flag = False
 try:
     from modules.test.ui import main as test
 
-    test_flag = not test_flag
+    test_flag = True
 except:
     print("Skip test")
 
