05c807005bd7677c75bba4eaeade7ce0e7ecc20c
newor0599 <rowen778623@gmail.com>
2025-08-12 00:57:03 +0800

    Added notification sound

--- scripts/system_notifications.py
+++ scripts/system_notifications.py
@@ -7,11 +7,22 @@ class SYSTEM_NOTIF:
     def __init__(self):
         self.service_inits()
         self.laptop_battery = self.upower.display_device
-        self.low_battery_notified = False
-        self.charging_notified = False
-        self.charged_notified = False
+        self.notified = {
+            "low bat": False,
+            "charging": False,
+            "charged": False,
+        }
+        self.battery_state = {
+            "max": 90,
+            "low": 15,
+            "lower": 5,
+        }
         self.laptop_battery.connect(
-            "notify::percent", lambda x, y: self.low_battery_notify()
+            "notify::percent",
+            lambda x, y: (
+                self.low_battery_notify(),
+                self.charged_notify(),
+            ),
         )
         self.laptop_battery.connect(
             "notify::charging", lambda x, y: self.charging_notify()
@@ -21,32 +32,59 @@ class SYSTEM_NOTIF:
     def service_inits(self):
         self.upower = UPowerService.get_default()
 
-    def notify(self, text: str, summary: str = "system"):
+    def notify(self, text: str, summary: str = "system", sound: bool = False):
         asyncio.create_task(
             Utils.exec_sh_async(f"notify-send -a System '{summary}' '{text}'")
         )
+        if sound:
+            asyncio.create_task(
+                Utils.exec_sh_async(
+                    "cvlc ~/.systemui/assets/sound/notification.mp3 --no-loop --no-repeat --play-and-exit"
+                )
+            )
 
     def low_battery_notify(self):
-        if self.laptop_battery.percent >= 90 and not self.charged_notified:
-            self.notify("Battery is fully charged!")
-            self.charged_notified = True
-            return
-        self.charged_notified = False
         if self.laptop_battery.charging:
             return
-        if self.laptop_battery.percent <= 5:
+
+        if self.laptop_battery.percent <= self.battery_state["lower"]:
             self.notify(
                 f"Battery is at {int(self.laptop_battery.percent)}%, charge now!"
             )
             return
-        if self.laptop_battery.percent <= 15 and not self.low_battery_notified:
-            self.notify(f"Battery is at {int(self.laptop_battery.percent)}%")
-            self.low_battery_notified = True
+
+        if (
+            self.laptop_battery.percent <= self.battery_state["low"]
+            and not self.notified["low bat"]
+        ):
+            self.notify(
+                f"Battery is at {int(self.laptop_battery.percent)}%",
+                sound=True,
+            )
+            self.notified["low bat"] = True
+
+        if self.laptop_battery.percent > self.battery_state["low"]:
+            self.notified["low bat"] = False
 
     def charging_notify(self):
-        if self.laptop_battery.charging and not self.charging_notified:
+        if self.laptop_battery.charging and not self.notifed["charging"]:
             self.notify("Battery is being charged!")
-            self.charging_notified = True
+            self.notified["charging"] = True
             return
-        if not self.laptop_battery.charging and self.charging_notified:
-            self.charging_notified = False
+        if not self.laptop_battery.charging:
+            self.notified["charging"] = False
+
+    def charged_notify(self):
+        if (
+            self.laptop_battery.percent >= self.battery_state["max"]
+            and not self.notified["charged"]
+        ):
+            self.notify("Battery is fully charged!")
+            self.notified["charged"] = True
+            return
+
+        if (
+            self.laptop_battery.percent < self.battery_state["max"]
+            or not self.laptop_battery.charging
+        ):
+            self.notified["charged"] = False
